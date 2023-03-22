6cd91a8a4ce6a3428d8e135611f0e5975fd50f80
m-moriss <103366111+m-moriss@users.noreply.github.com>
2023-03-22 16:34:07 +0100

    Update streamlit-prez.py

--- streamlit-prez.py
+++ streamlit-prez.py
@@ -1,6 +1,3 @@
-pip install streamlit-option-menu
-pip install beautifulsoup4 tensorflow
-
 import streamlit as st
 from streamlit_option_menu import option_menu
 import streamlit.components.v1 as html
@@ -16,7 +13,8 @@ from dataset import (
 import plotly.express as px
 import numpy as np
 
-
+pip install streamlit-option-menu
+pip install beautifulsoup4 tensorflow
 
 # Screens:
 # * Introduction du projet
