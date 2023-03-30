80dcfbc9079b6ed4ade1cd5fdcf260b34ad638b6
m-moriss <103366111+m-moriss@users.noreply.github.com>
2023-03-30 17:19:42 +0200

    Update streamlit_appw.py

--- streamlit_appw.py
+++ streamlit_appw.py
@@ -3,7 +3,7 @@ from streamlit_option_menu import option_menu
 import streamlit.components.v1 as html
 from PIL import Image, ImageOps
 import pandas as pd
-import datasets
+from datasets import load_dataset
 #from datasets import load_dataset
 #from datasets import load_vgg16_cnn_model
 #from datasets import process_text
