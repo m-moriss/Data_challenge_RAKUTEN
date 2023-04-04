eb554fa834f5bbd5f08cca9906bb1bff36e4438c
m-moriss <103366111+m-moriss@users.noreply.github.com>
2023-04-04 23:11:11 +0200

    Update donnees.py

--- donnees.py
+++ donnees.py
@@ -31,7 +31,8 @@ def strip_tags(html):
 @st.experimental_singleton
 def load_dataset():
     # On charge le dataset d'entrainement
-    X_train = pd.read_csv("X_train.csv",index_col=0)
+    dtypes = {"designation":"object", "description": "object", "productid":"int64", "imageid":"int64"}
+    X_train = pd.read_csv("X_train.csv",index_col=0,dtype = dtypes)
     y_train = pd.read_csv("Y_train.csv",index_col=0)
     categories = pd.read_csv("categories.csv", sep="\t")
 
