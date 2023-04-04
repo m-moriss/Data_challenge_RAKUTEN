60586a32f198d13ba774ef1dbc87cf4917727a42
m-moriss <103366111+m-moriss@users.noreply.github.com>
2023-04-04 23:16:27 +0200

    Update donnees.py

--- donnees.py
+++ donnees.py
@@ -46,7 +46,7 @@ def load_dataset():
     X = pd.concat([X_train, y_train], axis=1)
     X["categorie"] = X.prdtypecode.map(categories_numbered.number)
 
- #   X["image"] = "image_" + X.imageid.astype(str) + "_product_" + X.productid.astype(str) + ".jpg"
+    X["image"] = "image_" + X.imageid.astype(str) + "_product_" + X.productid.astype(str) + ".jpg"
  #   X["texte"] = X.designation + " " + X.description.fillna('').astype(str)
  #  X["orig_texte"] = X.designation + " " + X.description.fillna('').astype(str)
     #X.drop(["productid", "imageid", "prdtypecode", "designation", "description"], axis=1, inplace=True)
