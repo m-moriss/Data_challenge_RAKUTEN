99bd42b6e26ff5d279224da954f566c77cfc306b
m-moriss <103366111+m-moriss@users.noreply.github.com>
2023-04-05 14:52:53 +0200

    Update streamlit_appw.py

--- streamlit_appw.py
+++ streamlit_appw.py
@@ -229,7 +229,7 @@ RAKUTEN a mis à disposition les jeux de données organisés de la façon suivan
             index=0
             for i, row in sample.iterrows():
                
-                 filename = f"/image_train/image_{row.imageid}_product_{row.productid}.jpg"
+                 filename = f"image_train/image_{row.imageid}_product_{row.productid}.jpg"
                  image = Image.open(filename)
                  cols[index].image(image, use_column_width=True)
                  index += 1
