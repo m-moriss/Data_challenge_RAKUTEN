b71617414b4403baec3e9c654cfc856a8802ca23
m-moriss <103366111+m-moriss@users.noreply.github.com>
2023-04-05 09:57:43 +0200

    Update streamlit_appw.py

--- streamlit_appw.py
+++ streamlit_appw.py
@@ -229,7 +229,7 @@ RAKUTEN a mis à disposition les jeux de données organisés de la façon suivan
             for i, row in sample.iterrows():
                 filename = f"image_train/image_{row.imageid}_product_{row.productid}.jpg"
                 #filename = f"C:/Users/Morisseau1/DSPP/Donnees/images (1)/images/image_train/image_{row.imageid}_product_{row.productid}.jpg"
-                image = Image.open(filename,'rb')
+                image = Image.open(filename)
                 cols[index].image(image, use_column_width=True)
                 index += 1
 
