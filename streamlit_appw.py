2735cd2bbb159828274b728b236ec9f5cb22f82d
m-moriss <103366111+m-moriss@users.noreply.github.com>
2023-04-07 11:34:37 +0200

    Update streamlit_appw.py

--- streamlit_appw.py
+++ streamlit_appw.py
@@ -268,14 +268,14 @@ Résultat : **nous ajouterons une variable nommé "catégorie" qui pourra faire 
     tabs = st.tabs(list(categories.prdlabelcode.array))
 
     nb_sample = 3
-    for i in range(0, len(categories)):
+    for i in range(1, len(categories)):
         with tabs[i]:
             st.markdown(f"<p><strong>Exemples d'annonces de la catégorie {categories.iloc[i].prdlabelcode}</strong></p>", unsafe_allow_html=True)
             category = categories.iloc[i].prdtypecode
             sample = X[y_train.prdtypecode == category].sample(nb_sample)
             cols = st.columns([1]*nb_sample)
             index=0
-            for i, row in sample.iterrows():
+            for indice, row in sample.iterrows():
                 filename = f"img_train/image_{row.imageid}_product_{row.productid}.webp"
                 #filename = f"C:/Users/Morisseau1/DSPP/Donnees/images (1)/images/image_train/image_{row.imageid}_product_{row.productid}.jpg"
                 image = Image.open(filename)
