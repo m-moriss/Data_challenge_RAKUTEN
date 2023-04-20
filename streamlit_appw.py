e38a32680db1a368eb5e57fae681c09aa05dad9d
m-moriss <103366111+m-moriss@users.noreply.github.com>
2023-04-20 11:44:13 +0200

    Update streamlit_appw.py

--- streamlit_appw.py
+++ streamlit_appw.py
@@ -10,8 +10,8 @@ import pathlib
 from donnees import (
     load_dataset,
     process_text,
-    load_vgg16_cnn_model,
-    get_image,
+    #load_vgg16_cnn_model,
+    #get_image,
     load_logisitc_regression_model
 )
 
