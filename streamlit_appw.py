eff489423b2c04722908fbef97a35e990b943c4a
m-moriss <103366111+m-moriss@users.noreply.github.com>
2023-04-20 12:41:39 +0200

    Update streamlit_appw.py

--- streamlit_appw.py
+++ streamlit_appw.py
@@ -622,9 +622,13 @@ if choose == "Bilan":
     
     st.markdown("<p class=\"page_title_2\">Apport du projet et de la formation</p>", unsafe_allow_html=True)
     
-    st.write("""Ce projet a été l'occasion pour les membres du groupe de **renforcer leurs connaissances théoriques et d'apprendre les usages de nouveaux outils.**
-    **Martine**, Data Analyst et expérimentée dans l'exploitation de base de données, a pu se familiariser au **langage Python et aux analyses 
-    de données textuelles et images**.
-    **Rémi**, ingénieur informatique a davantage développer ses connaissances de **traitement de données et de principes statistiques à des fins opérationnelles**.
+    st.write("""Cette formation a été l'occasion de découvrir ou enrichir nos compétences dans le secteur de la Data et de developper des compétences techniques de plus en plus
+    sollicités dans le monde professionnel. Notre curiosité nous a poussé à choisir un projet différent de ce que nous pouvions connaitre (Martine étant familière de l'exploitation
+    de données quantitatives ou de données issues de bases de données, découverte de la modélisation statistiques pour Rémi).
+    
+    Ce projet a été l'occasion pour les membres du groupe de **renforcer leurs connaissances théoriques et d'apprendre les usages de nouveaux outils.**
+    Indéniablement, ce projet occupe une place importante dans la formation, nous permettant de mettre en pratique l'ensemble des notions vues dans un contexte
+    quasi-opérationnel.
+
   
     """) 
